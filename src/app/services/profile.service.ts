47af6f9f12d43b93193a281d526f820f116042ee
DenisMuga <denismugah5@gmail.com>
2022-04-17 12:05:41 +0300

    chore: adding and initializing tokens to profile services

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';  
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username:string;
+  private clientid = '252a21e805fec95ffc2a';
+  private clientsecret = '167b7081cadd75169a679cf315fb7ce7881a49eb';
+
+  constructor(private http:Http) { 
+    console.log("Service is now ready!");
+    this.username = "DenisMuga";
+  }
 }
