c119f5cd721513edfb2565a2e152f80c1bd2223e
DenisMuga <denismugah5@gmail.com>
2022-04-19 15:34:09 +0300

    chore: adding routing module utility

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"profiles",pathMatch:"full"},
+  {path:"profiles", component:ProfileComponent},
+  {path:"repos", component:RepositoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
